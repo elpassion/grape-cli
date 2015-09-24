0b1539b9c2e432bd01a971740b0f3b84f4046da2
ghostbuster91 <kghost0@gmail.com>
2015-09-24 15:25:26 +0200

    use thor to generate directory and gem file

--- lib/grape/cli/new.rb
+++ lib/grape/cli/new.rb
@@ -5,3 +5,17 @@ class GrapeCLI < Thor
     puts "#{app_name}"
   end
 end
+
+class ProjectGenerator < Thor
+  include Thor::Actions
+
+  desc "new PROJECT_NAME", "some desc to avoid warning"
+
+  def self.source_root
+    File.dirname(__FILE__)
+  end
+
+  def generate(folder, app_name)
+    directory('template', File.join(folder, app_name))
+  end
+end
--- test/grape/create_test.rb
+++ test/grape/create_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
 require 'tmpdir'
+require './lib/grape/cli'
 
 class Grape::CreateTest < Minitest::Test
   def setup
@@ -19,22 +20,5 @@ class Grape::CreateTest < Minitest::Test
 
   def test_creates_a_gemfile
     assert File.exists?(File.join(@app_folder, 'Gemfile'))
-  end
-end
-
-class ProjectGenerator < Thor
-  include Thor::Actions
-
-  class_option :app_name, default: 'my_app'
-
-  desc "new PROJECT_NAME", "some desc to avoid warning"
-
-  def self.source_root
-    File.dirname(__FILE__)
-  end
-
-  def generate(folder, app_name)
-    Dir.mkdir(File.join(folder, app_name))
-    FileUtils.touch(File.join(folder, app_name, 'Gemfile'))
   end
 end
