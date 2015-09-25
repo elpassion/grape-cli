cc9919687d7ca3d1e7c7ebd3ac57dad659dd5ae9
ghostbuster91 <kghost0@gmail.com>
2015-09-25 16:35:11 +0200

    This adds funcionality to use multiple databases. This closes #12

--- lib/grape/cli/new.rb
+++ lib/grape/cli/new.rb
@@ -7,18 +7,21 @@ class GrapeCli < Thor
   desc "new APP_NAME", "Create a new Grape application"
 
   method_option :work_dir, default: Dir.pwd
+  method_option :database, aliases: 'd', default: 'postgres'
 
   def self.source_root
     File.dirname(__FILE__)
   end
 
   def new(app_name)
-    application_template_path = File.join(GrapeCli.source_root, 'template/application')
-    destination_path          = File.join(options[:work_dir], app_name)
-    config                    = create_config(app_name)
+    template_path = File.join(GrapeCli.source_root, 'template')
+    application_template_path = File.join(template_path, 'application')
+    destination_path = File.join(options[:work_dir], app_name)
+    config = create_config(app_name)
 
     directory(application_template_path, destination_path, config)
-
+    template(File.join(template_path, 'database', "#{options[:database]}.yml.tt"),
+             File.join(destination_path, 'config', 'database.yml'))
     inside destination_path do
       run(ApplicationFactory.instance.command_generator.bundle_install, config)
     end
@@ -29,8 +32,18 @@ class GrapeCli < Thor
   def create_config(app_name)
     {
       app_name: app_name,
-      verbose: ApplicationFactory.instance.verbose_output,
-      class_name: ClassNameGenerator.new(app_name).generate
+      class_name: ClassNameGenerator.new(app_name).generate,
+      database: options[:database],
+      database_gem: database_gem(options[:database]),
+      verbose: ApplicationFactory.instance.verbose_output
     }
+  end
+
+
+  def database_gem(database)
+    {
+        postgres: 'pg',
+        sqlite: 'sqlite3'
+    }[database.to_sym]
   end
 end
