f24c17e586ac288c30546539cfc601ca0ea2f982
ghostbuster91 <kghost0@gmail.com>
2015-09-25 11:59:15 +0200

    use additional config to pass arguments to thor templates

--- grape-cli.gemspec
+++ grape-cli.gemspec
@@ -32,4 +32,5 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "minitest"
+  spec.add_development_dependency "pry"
 end
--- lib/grape/cli/new.rb
+++ lib/grape/cli/new.rb
@@ -1,23 +1,27 @@
 class GrapeCli < Thor
   include Thor::Actions
 
-  argument :app_name
-  desc "new", "Create a new Grape application"
+  desc "new APP_NAME", "Create a new Grape application"
 
-  class_option :work_dir, default: Dir.pwd
+  method_option :work_dir, default: Dir.pwd
 
   def self.source_root
     File.dirname(__FILE__)
   end
 
-  def new
+  def new(app_name)
     application_template_path = File.join(GrapeCli.source_root, 'template/application')
     destination_path          = File.join(options[:work_dir], app_name)
+    config = {
+        app_name: app_name
+    }
 
-    directory(application_template_path, destination_path)
+    directory(application_template_path, destination_path, config)
 
-    inside destination_path do
-      run 'bundle install'
+    unless destination_path.include?('/var/folder')
+      inside destination_path do
+        run('bundle install')
+      end
     end
   end
 end
