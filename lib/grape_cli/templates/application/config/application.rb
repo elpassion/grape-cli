1e359e5fb44a21f609825667c0fb2c1bf782cf17
wswidzinski <swidzinskiw@gmail.com>
2016-06-27 15:14:01 +0200

    Fix activerecord initializing

--- lib/grape_cli/templates/application/config/application.rb
+++ lib/grape_cli/templates/application/config/application.rb
@@ -5,6 +5,8 @@ require 'boot'
 
 Bundler.require :default, ENV['RACK_ENV']
 
+Grape::ActiveRecord.configure_from_file!(File.expand_path('../database.yml', __FILE__))
+
 Dir[File.expand_path('../../app/models/*.rb', __FILE__)].each { |file| require file }
 Dir[File.expand_path('../../app/resources/*.rb', __FILE__)].each { |file| require file }
 
